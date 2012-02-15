b1d4a7899356b5c70241f7da4cf76f2598e80f0e
Maciej Hryniszak <maciej.hryniszak@lumesse.com>
2012-02-15 12:16:53 +0100

    Upgraded release plugin to 1.0.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,8 +14,6 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:1.0.0") {
-            export = false
-        }
+        build(":release:1.0.1") { export = false }
     }
 }
